2aeb7611274e5090b7096cb0bca481303c8791d2
Istiaque Mahbub Isti <you@example.comistiaquemahbub1@gmail.com>
2024-10-21 22:00:46 +0600

    blog data showed in the site

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,16 +1,20 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs]=useState([]);
     useEffect(()=>{
         fetch('blogs.json')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setBlogs(data))
     },[])
     return (
         <div className="md:w-2/3">
             <h2 className="text-4xl">Blogs</h2>
+            {
+                blogs.map(blog=><Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
