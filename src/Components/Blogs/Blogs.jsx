a0d8efe47e202e79e67200dd78ac397dc09c7362
Istiaque Mahbub Isti <you@example.comistiaquemahbub1@gmail.com>
2024-10-21 21:27:37 +0600

    Header,Blogs,Blog components are created config tailwind css fetch data from blogs.json flie

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+    const [blogs, setBlogs]=useState([]);
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
+    return (
+        <div className="md:w-2/3">
+            <h2 className="text-4xl">Blogs</h2>
+        </div>
+    );
+};
+
+export default Blogs;
